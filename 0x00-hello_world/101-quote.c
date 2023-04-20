bf3b9cd3cb9ffc07e46913cfbd42d327c69c5d52
Makwena <maleka.emkay@gmail.com>
2023-04-20 06:09:08 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
 * main - Entry point*
-* Return: Always 0
+* Return: Always 1
 */
 
 int main(void)
 {
-	write(1,
+	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
